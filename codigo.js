e18b4ea2e150bc8862a6d981e552ec9699b65033
mile digiacomo <milenadigiacomo@outlook.es>
2022-08-04 14:09:01 +0200

    .

--- codigo.js
+++ codigo.js
@@ -1,32 +1,19 @@
-const bikinis=[
-    {
-        nombre:"mara",
-        precio:"$3450"
-    },
-    {
-        nombre:"raquel",
-        precio:"$2700"
-    },
-    {
-        nombre:"mili",
-        precio:"$3350"
-    },
-    {
-        nombre:"nicole",
-        precio:"$3800"
-    }
-];
+const bikinis=[]
 
-for(const bikini of bikinis){
-    console.log(bikini.nombre);
-    console.log(bikini.precio);
+function busqueBikini(){
+    var order= [];
+    do{
+        nombre=prompt("ingrese el nombre de la bikini(mili, raquel, nicole, mara)");
+        if(nombre==""){
+            break;
+        }
+        order.push(nombre)
+        let talle=prompt("ingrese la talla (xs, s, m, l");
+        order.push(talle)
+        let cantidad=prompt("ingrese la cantidad");
+        order.push({ nomb: nombre, size: talle, count:cantidad });
+    }while (nombre!="");
+    console.log(order)
 }
 
-let bikiniABuscar=prompt("Ingresa la bikini que desea (mara, mili, nicole, raquel");
-for(const bikini of bikinis){
-    if(bikini.nombre==bikiniABuscar){
-        let posicion=bikinis.indexOf(bikini);
-        alert("La bikini se encuentra en la posicion "+posicion);
-        console.log(bikini);
-    }
-}
+busqueBikini();
